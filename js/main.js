79b0144e6f948964d6142fea75f0e16fad696122
Lulalalu <lulalalu9696@gmail.com>
2023-05-22 12:25:08 +0800

    js icon x solve

--- js/main.js
+++ js/main.js
@@ -1,21 +1,10 @@
-const toggleBtn = document.querySelector('.toggle_btn')
-const toggltBtnIon = document.querySelector('.toggle_btn i')
-const dropDownMenu = document.querySelector('.dropdown_menu')
+const toggleBtn = document.querySelector('.toggle_btn');
+const toggleBtnIcon = document.querySelector('.toggle_btn i');
+const dropDownMenu = document.querySelector('.dropdown_menu');
 
 toggleBtn.onclick = function () {
   dropDownMenu.classList.toggle('open')
+  const isOpen = dropDownMenu.classList.contains('open')
+    ? 'fa-solid fa-x'
+    : 'fa-solid fa-bars'
 }
-
-
-// const toggleBtn = document.querySelector('.toggle_btn')
-// const toggleBtnIon = document.querySelector('.toggle_btn i')
-// const dropDownMenu = document.querySelector('.dropdown_menu')
-//
-// toggleBtn.onclick = function () {
-//   dropDownMenu.classList.toggle('open')
-//   const isOpen = dropDownMenu.classList.contains('open')
-//
-//   toggleBtnIon.classList = isOpen
-//     ?'fa-solid fa-xmark'
-//     :'fa-solid fa-bars'
-// }
